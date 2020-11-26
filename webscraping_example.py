5853cdf444b662d080686643a383e64ec1ee3b03
robert-g-butler <48664123+robert-g-butler@users.noreply.github.com>
2020-11-26 13:45:42 -0800

    Added wait conditions and more dropdown updates

--- webscraping_example.py
+++ webscraping_example.py
@@ -1,8 +1,10 @@
 
-import pathlib as Path
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 DRIVER_FILEPATH = r'C:\Users\robbi\Downloads\webscraping_example\chromedriver_v87.exe'
 NEW_DOWNLOADS_FOLDERPATH = r'C:\Users\robbi\Downloads\webscraping_example'
@@ -23,21 +25,50 @@ driver = webdriver.Chrome(executable_path=DRIVER_FILEPATH,
 
 # Go to the website
 driver.get('https://www.sgx.com/research-education/derivatives')
-time.sleep(5)
 
-# Update "Time and Sales Historical Data", "Type of Data" to "Trade Cancellation"
-type_of_data_dropdown = driver.find_element_by_xpath('//*[@id="page-container"]/template-base/div/div/section[1]/div/sgx-widgets-wrapper/widget-research-and-reports-download[1]/widget-reports-derivatives-tick-and-trade-cancellation/div/sgx-input-select[1]/label/span[2]/input')
-type_of_data_dropdown.click()
-time.sleep(5)
+# Specify the xpaths of the "Time and Sales Historical Data" dropdowns and download button
+TYPE_DROPDOWN_XPATH = '//*[@id="page-container"]/template-base/div/div/section[1]/div/sgx-widgets-wrapper/widget-research-and-reports-download[1]/widget-reports-derivatives-tick-and-trade-cancellation/div/sgx-input-select[1]/label/span[2]/input'
+TYPE_OPTION_XPATH = '//*[@id="sgx-select-dialog"]/div[2]/sgx-select-picker/sgx-list/div/div/sgx-select-picker-option[3]'
 
-trade_cancellation_option = driver.find_element_by_xpath('//*[@id="sgx-select-dialog"]/div[2]/sgx-select-picker/sgx-list/div/div/sgx-select-picker-option[3]')
-trade_cancellation_option.click()
-time.sleep(5)
+DATE_DROPDOWN_XPATH = '//*[@id="page-container"]/template-base/div/div/section[1]/div/sgx-widgets-wrapper/widget-research-and-reports-download[1]/widget-reports-derivatives-tick-and-trade-cancellation/div/sgx-input-select[2]/label/span[2]/input'
+DATE_OPTION_XPATH = '//*[@id="sgx-select-dialog"]/div[2]/sgx-select-picker/sgx-list/div/div/sgx-select-picker-option[1]'
 
-# Download "Time and Sales Historical Data"
-download_button = driver.find_element_by_xpath('//*[@id="page-container"]/template-base/div/div/section[1]/div/sgx-widgets-wrapper/widget-research-and-reports-download[1]/widget-reports-derivatives-tick-and-trade-cancellation/div/button')
+DOWNLOAD_BUTTON_XPATH = '//*[@id="page-container"]/template-base/div/div/section[1]/div/sgx-widgets-wrapper/widget-research-and-reports-download[1]/widget-reports-derivatives-tick-and-trade-cancellation/div/button'
+
+
+
+# Open the "Type of Data" dropdown menu by clicking on it
+type_dropdown = WebDriverWait(driver=driver, timeout=10).until(EC.element_to_be_clickable((By.XPATH, TYPE_DROPDOWN_XPATH)))
+time.sleep(1)
+type_dropdown.click()
+
+# Click the "Trade Cancellation" option
+type_option = WebDriverWait(driver=driver, timeout=10).until(EC.element_to_be_clickable((By.XPATH, TYPE_OPTION_XPATH)))
+time.sleep(1)
+type_option.click()
+
+
+
+# Open the "Date" dropdown menu by clicking on it
+date_dropdown = WebDriverWait(driver=driver, timeout=10).until(EC.element_to_be_clickable((By.XPATH, DATE_DROPDOWN_XPATH)))
+time.sleep(1)
+date_dropdown.click()
+
+# Click the "Trade Cancellation" option
+date_option = WebDriverWait(driver=driver, timeout=10).until(EC.element_to_be_clickable((By.XPATH, DATE_OPTION_XPATH)))
+time.sleep(1)
+date_option.click()
+
+
+
+# Click the download button
+download_button = WebDriverWait(driver=driver, timeout=10).until(EC.element_to_be_clickable((By.XPATH, DOWNLOAD_BUTTON_XPATH)))
+time.sleep(1)
 download_button.click()
 time.sleep(5)
 
+
+
 # Close the Chrome browser
 driver.close()
+
